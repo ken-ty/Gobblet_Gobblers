11fdb2d4825aa4f9426a7d6d3e0623acb8a48a60
kentokura <s16c1089vn@s.chibakoudai.jp>
2019-11-27 16:23:40 +0900

    Update player_ai.py

--- player_ai.py
+++ player_ai.py
@@ -1,5 +1,5 @@
 import tictactoe as tic
-import algorithm as algo
+import statevalue as value
 
 def action( state, mode="Random" )
   """現在の状態から思考ルーチンに沿って行動を生成する.
@@ -13,23 +13,20 @@ def action( state, mode="Random" )
   
   if mode = "Random":
     # 合法手を取得し、その中からランダムに行動を選択する.
-    """
     legal_actions = state.legal_actions()
     random_action = legal_actions[random.randint(0, len(legal_actions)-1)]
     return random_action
-    """
-    random_action = algo.random_action( state )
-    return random_action
+
 
   elif mode = "MiniMax:
     score       = -float( 'inf' ) #  行動の価値.
     best_score  = -float( 'inf' ) # 最も高い行動の価値.
     best_action = 0               # 最も価値の高い行動.
 
-    # 全ての合法手に対して、価値を計算.
+    # 全ての合法手を取得し、最も価値が高い行動を選択する.
     for action in state.legal_actions():
         # 価値を取得
-        score = algo.mini_max_action( state.next( action ) )
+        score = value.mini_max( state.next( action ) )
         # 価値が高いなら更新
         if score > best_score:
             best_action = action
