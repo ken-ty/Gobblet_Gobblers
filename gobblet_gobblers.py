d0f77d8826ee510174af171402e63b3ec7fdb765
kentokura <s16c1089vn@s.chibakoudai.jp>
2019-11-29 02:12:30 +0900

    Update gobblet_gobblers.py

--- gobblet_gobblers.py
+++ gobblet_gobblers.py
@@ -19,7 +19,7 @@ class State:
     __str__(self): 文字列表示
   """
 
-  def __init__(self, my_pieces=None, enemy_pieces=None, my_toplayer_pieces=None, enemy_toplayer_pieces=None, player=1 ):
+  def __init__(self, my_pieces=None, enemy_pieces=None, player=1 ):
     """初期化
     引数で与えられた石から現在の局面を作成する.
     
@@ -31,15 +31,56 @@ class State:
       player (int): 先手は1, 後手は-1を入力
     """
     # 全ての駒の位置(0|1)
-    self.my_pieces    = my_pieces    if my_pieces    != None else [0] * 9 * 3
-    self.enemy_pieces = enemy_pieces if enemy_pieces != None else [0] * 9 * 3
-    
-    # 盤面に見えてる駒の状態(0|1|2|3), 1から小さい順.
-    self.my_toplayer_pieces    = my_toplayer_pieces    if my_toplayer_pieces    != None else [0] * 9
-    self.enemy_toplayer_pieces = enemy_toplayer_pieces if enemy_toplayer_pieces != None else [0] * 9
+    self.my_pieces    = my_pieces    if my_pieces    != None else [0] * 27
+    self.enemy_pieces = enemy_pieces if enemy_pieces != None else [0] * 27
     
     # 先手か後手か
     self.player = player
+    
+    # 盤面に見えてる駒の状態(0|1|2|3), 1から小さい順.
+    my_toplayer_pieces    = [0] * 9
+    enemy_toplayer_pieces = [0] * 9
+    for i in range(9):
+      for j in range(0, 3):
+      if self.my_pieces[i+9*j] != 0:
+        my_toplayer_pieces[i]    = j+1
+      elif self.enemy_pieces[i+9*j] != 0:
+        enemy_toplayer_pieces[i] = j+1
+    self.my_toplayer_pieces    = my_toplayer_pieces
+    self.enemy_toplayer_pieces = enemy_toplayer_pieces
+    
+    # 手駒(1|2|3)のリスト.
+    hand_my_pieces = []
+    for i in range(3):
+      num_use_my_pieces = my_pieces[9*(i-1):9*i].count(i+1)
+      num_not_use_my_pieces = 2 - num_use_my_pieces
+      for num in num_not_use_my_pieces:       
+        hand_my_pieces.append(i+1)
+    self.hand_my_pieces = hand_my_pieces
+    
+    # 盤上の動かせる駒(0~26, 手駒から出せるなら-1も加える.)
+    can_move_my_pieces = None
+    # 0(駒なし),2,3は、toplayerをコピーでOK.
+    can_move_my_pieces_candidates = self.my_toplayer_pieces.copy()
+    # 1の駒のみ, 移動先のマスがあるか確認.
+    for i in range(9):
+      if can_move_my_pieces_candidates[i] == 1:
+        not_put = 1
+        for j in range(9):
+          if self.my_toplayer_pieces[j] == 0 and self.enemy_toplayer_pieces[j] == 0:
+            not_put = 0
+            break;
+        if not_put:
+          can_move_my_pieces_candidates[i] = 0:
+    # 候補を0~26のマスに直す.
+    for i in range(9):
+      if can_move_my_pieces_candidates[i] > 0:
+        can_move_my_pieces.append(i+9*(can_move_my_pieces_candidates[i] -1))
+    # 手駒を使い切ってなければ-1を加える.
+    if len(self.hand_my_pieces) < 6
+      can_move_my_pieces.append.append(-1)
+    # ソートする.
+    can_move_my_pieces.sort()
     
   def is_lose(self):
     """敗北判定
