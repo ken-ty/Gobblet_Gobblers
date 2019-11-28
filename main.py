d425e57fe09893630286694897799871127561e8
kentokura <s16c1089vn@s.chibakoudai.jp>
2019-11-28 11:00:00 +0900

    add comment

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""行動を変えてみよう
+ai.action(state, mode="ココ！")
+modeの一覧:
+    Random:  ランダムな行動選択
+    MiniMax: MiniMaxで計算した価値に基づいて行動選択
+"""
+
 import tictactoe as tic # クラスStateを定義.
 import player_ai as ai  # ゲームAI.ミニマックスによる行動.ランダムな行動.
 import random
@@ -16,11 +23,11 @@ while ( state.is_done() != True ) :
     # p1の行動選択
     if player == 1:
         print("Random Player\n" )
-        action = ai.action( state, mode="MiniMax" )
+        action = ai.action( state, mode="Random" )
     # p2の行動選択
     if player == -1:
         print("MiniMax Player\n" )
-        action = ai.action( state, mode="MiniMax"   )
+        action = ai.action( state, mode="MiniMax" )
         
     # 行動を状態に反映させた次の状態に更新する。
     state = state.next( action )
