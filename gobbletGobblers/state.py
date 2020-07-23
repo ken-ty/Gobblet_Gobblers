9d8a5065f7668aab1a750f50335bc0dc0d52ad64
toku <healthy1109@gmail.com>
2020-07-23 21:49:34 +0900

    作業を保存

--- gobbletGobblers/state.py
+++ gobbletGobblers/state.py
@@ -1,20 +1,7 @@
 # coding=utf-8
+def hoge():
+    return 0
+
+
 class State:
-    """ステート
-      listにする場合は、hoge_actionsと命名します。
-    """
-
-    def __init__(self, black, white):
-        """
-        Args:
-          black (int): 動かす駒の元の位置.-1, 0~26で指定.手駒からは-1.
-          white (int): 駒を動かす先の位置.0~26で指定.
-        """
-        self.black = black
-        self.white = white
-
-    def before_put_position(self):
-        return self.before_put_position
-
-    def _put_position(self):
-        return self.put_position
+    pass
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,10 +1,10 @@
-import unittest
-import state
+from unittest import TestCase
+from gobbletGobblers.state import State
 
 
-class TestState(unittest.TestCase):
+class TestState(TestCase):
     def test_encode(self):
         val = 10
         expect = "10"
-        actual = state.encode(val)
+        actual = State.encode(val)
         self.assertEqual(expect, actual)
