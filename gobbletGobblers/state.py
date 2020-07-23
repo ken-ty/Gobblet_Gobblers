674ed3376b9439f1d1e4b59a4c006c872fb88aa9
toku <healthy1109@gmail.com>
2020-07-24 03:43:13 +0900

    作業を保存

--- gobbletGobblers/state.py
+++ gobbletGobblers/state.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+import numpy as np
 
 
 class State:
-    def __init__(self):
-        pass
+    def __init__(self, black=None, white=None):
+        self.black = black if black is not None else np.uint32(0x000000000)
+        self.white = white if white is not None else np.uint32(0x000000000)
 
     def encode(self):
         return "hoge"
--- tests/test_state.py
+++ tests/test_state.py
@@ -3,13 +3,32 @@ import gobbletGobblers.state
 
 
 class TestState(unittest.TestCase):
-    def test_encode(self):
+    def test___init__(self):
+        patterns = [
+            ((None, None), (0, 0)),
+            ((None, None), (0, 0)),
+            ((None, None), (0, 0)),
+        ]
+        for input_param, expect_param in patterns:
+            with self.subTest('hogehoge'):
+                black, white = input_param
+                state = gobbletGobblers.state.State(black, white)
+                expect_black, expect_white = expect_param
+                expect = expect_black, expect_white
+                actual = state.black, state.white
+                self.assertEqual(expect, actual)
+
+    def test___init__2(self):
         state = gobbletGobblers.state.State()
+        expect = 0
+        actual = state.white
+        self.assertEqual(expect, actual)
+
+    def test_encode(self, state=state):
         val = 10
         expect = "hoge"
         actual = state.encode()
         self.assertEqual(expect, actual)
-
 
 if __name__ == '__main__':
     unittest.main()
